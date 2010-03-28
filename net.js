390eaf23398ba3b83500a8c5256221e0955a9e8f
troyand <troyanovsky@gmail.com>
2010-03-28 15:06:30 +0300

    Revert "Revert "minor net.js changes""
    This reverts commit f3536479af856318677031b0ae31660bbcaf334c.

--- net.js
+++ net.js
@@ -1,9 +1,9 @@
 var net = {
     alphabetSize: 10,
-    hiddenLayerSize: 15,
+    hiddenLayerSize: 5,
     initialWeightAbsMax: 0.2,
-    imgWidth: 20,
-    imgHeight: 30,
+    imgWidth: 3,
+    imgHeight: 5,
     eta: 0.4,
     sigmoid: function(x){
         return 1/(1 + Math.exp(-x));
@@ -34,7 +34,7 @@ var net = {
             for(var j = 0; j < this.imgWidth*this.imgHeight; j++){
                 out1[i] += this.weights1[j][i] * vector[j];
             }
-            out1[i] = Math.round(this.sigmoid(out1[i]));
+            out1[i] = this.sigmoid(out1[i]);
         }
         console.log(out1);
         var out2 = [];
@@ -70,13 +70,12 @@ var net = {
                 this.weights1[j][i] += delta * vector[j] * this.eta;
             }
         }
-        //console.log(this.weights2);
     }
 };
 
 
 function makeImage(n, img){
-    img.src = n+'a.png';
+    img.src = n+'.png';
     img.onload = function(){
         var canvas = $('<canvas/>', {
             id: 'number'+n,
